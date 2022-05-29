2f4ec8188ca3b237fbe4cb296dffb1b477fe617c
luey k <>
2022-05-29 06:48:30 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const aoijs = require("aoi.js")
+const ashy = new aoijs.Bot({
+token: "OTgwMzUxODA3Nzk4Nzk2MzMw.GOY66F.dEtOPRVveTqoSonUQM9F4ca3b4lzC42cmfn5YM",
+prefix: "a!",
+intents: ["GUILDS", "GUILD_MESSAGES"]
+})
+
+ashy.onMessage()
